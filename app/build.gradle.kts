0a28cb85985ec0b116bf248c7fee694a8b15f5b5
Nathan Campos <nathan@innoveworkshop.com>
2023-11-12 12:51:43 +0000

    Fixed the compilation target in the build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.innoveworkshop.todoer"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.innoveworkshop.todoer"
