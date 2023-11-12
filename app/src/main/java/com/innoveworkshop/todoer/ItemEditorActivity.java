d3be756fdb1cfa78e4c77784b297cf840a1cf194
Nathan Campos <nathan@innoveworkshop.com>
2023-11-12 12:52:25 +0000

    Added the menus to the ActionBars and initial logic

--- app/src/main/java/com/innoveworkshop/todoer/ItemEditorActivity.java
+++ app/src/main/java/com/innoveworkshop/todoer/ItemEditorActivity.java
@@ -1,8 +1,12 @@
 package com.innoveworkshop.todoer;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class ItemEditorActivity extends AppCompatActivity {
 
@@ -10,5 +14,29 @@ public class ItemEditorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item_editor);
+
+        setupComponents();
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_item_editor, menu);
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.action_save_item) {
+            // ActionBar "Save" button.
+            finish();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void setupComponents() {
+        // Setup the ActionBar.
+        setSupportActionBar(findViewById(R.id.toolbar));
     }
 }
--- app/src/main/java/com/innoveworkshop/todoer/MainActivity.java
+++ app/src/main/java/com/innoveworkshop/todoer/MainActivity.java
@@ -1,8 +1,12 @@
 package com.innoveworkshop.todoer;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setupComponents();
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.action_new_item) {
+            // ActionBar "Add" button.
+            Intent intent = new Intent(MainActivity.this, ItemEditorActivity.class);
+            startActivity(intent);
+
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void setupComponents() {
+        // Setup the ActionBar.
+        setSupportActionBar(findViewById(R.id.toolbar));
     }
 }
