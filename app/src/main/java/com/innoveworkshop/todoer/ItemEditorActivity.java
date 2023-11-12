849f3e62d6122fdddc57be0103e1d0c804717142
Nathan Campos <nathan@innoveworkshop.com>
2023-11-12 12:00:26 +0000

    Created the basic layout for the main and edit activities

---
+++ app/src/main/java/com/innoveworkshop/todoer/ItemEditorActivity.java
@@ -0,0 +1,14 @@
+package com.innoveworkshop.todoer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ItemEditorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_editor);
+    }
+}
